8417754b47aa453ec60987aa793de0c82ab179ab
Narathip Harijiratiwong <tholop@gmail.com>
2025-12-12 23:39:14 +0700

    update tsconfig and setting to use typescript 5.9

--- packages/backend/src/infrastructure/models/index.ts
+++ packages/backend/src/infrastructure/models/index.ts
@@ -1,22 +1,22 @@
 // User models
-export { User } from './User';
 export { CreditTransaction } from './CreditTransaction';
+export { User } from './User';
 export { UserAchievement } from './UserAchievement';
 export { UserAvatar } from './UserAvatar';
-export { UserStoryProgress } from './UserStoryProgress';
 export { UserFavorite } from './UserFavorite';
 export { UserSession } from './UserSession';
+export { UserStoryProgress } from './UserStoryProgress';
 
 // Story models
-export { Story } from './Story';
-export { StoryNode } from './StoryNode';
-export { StoryAsset } from './StoryAsset';
-export { StoryGallery } from './StoryGallery';
 export { Genre } from './Genre';
 export { Review } from './Review';
 export { ReviewFlag } from './ReviewFlag';
 export { ReviewVote } from './ReviewVote';
+export { Story } from './Story';
+export { StoryAsset } from './StoryAsset';
 export { StoryFlag } from './StoryFlag';
+export { StoryGallery } from './StoryGallery';
+export { StoryNode } from './StoryNode';
 
 // Achievement models
 export { Achievement } from './Achievement';
@@ -24,8 +24,8 @@ export { Avatar } from './Avatar';
 
 // Admin models
 export { AdminAccount } from './AdminAccount';
-export { AdminLoginHistory } from './AdminLoginHistory';
 export { AdminLog } from './AdminLog';
+export { AdminLoginHistory } from './AdminLoginHistory';
 
 // System models
 export { Analytics } from './Analytics';
